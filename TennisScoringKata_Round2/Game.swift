3364c746578ddf75e9d2ea34b5b25a11e1d14de6
Daniel Reinhart <Danielr@procore.com>
2017-05-19 09:53:25 -0700

    Set up with initial classes.

---
+++ TennisScoringKata_Round2/Game.swift
@@ -0,0 +1,17 @@
+//
+//  Game.swift
+//  TennisScoringKata_Round2
+//
+//  Created by Daniel Reinhart on 5/19/17.
+//  Copyright © 2017 Daniel Reinhart. All rights reserved.
+//
+
+import Foundation
+
+class Game {
+
+    func score() -> String {
+        return "0 - 0"
+    }
+
+}
