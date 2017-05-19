94121062856de7387d0f01a580c92298c8313ba0
Daniel Reinhart <Danielr@procore.com>
2017-05-19 09:49:50 -0700

    Initial Commit

---
+++ TennisScoringKata_Round2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TennisScoringKata_Round2
+//
+//  Created by Daniel Reinhart on 5/19/17.
+//  Copyright © 2017 Daniel Reinhart. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
